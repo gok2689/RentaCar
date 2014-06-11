f05fed45003ea27f27685a5830598f7d9b99ecc3
goktug <goktug.dulkan@gmail.com>
2014-06-11 15:59:49 +0300

    https://github.com/gok2689/RentaCar.git

---
+++ RentaCar/Controllers/BrandController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RentaCar.Controllers
+{
+    public class BrandController : Controller
+    {
+        //
+        // GET: /Brand/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
