3e6a05887ad275e306570af5c9f49d382dce9fb1
goktug <goktug.dulkan@gmail.com>
2014-06-12 17:17:13 +0300

    https://github.com/gok2689/RentaCar.git

--- RentaCar/Controllers/MemberController.cs
+++ RentaCar/Controllers/MemberController.cs
@@ -1,19 +1,33 @@
-﻿using System;
+﻿using RentaCarEntities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using RentaCarBll;
 
 namespace RentaCar.Controllers
 {
     public class MemberController : BaseController
     {
-        //
-        // GET: /Member/
+       MemberManager _manager =new MemberManager();
 
         public ActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult EditMember(Member member)
+        {
+            if (ModelState.IsValid)
+            {
+                _manager.Add(member);
+                ViewBag.Result = "Kayıdınız yapıldı";
+            }
+
+            return RedirectToAction("Index", "Home");
+        
+        
         }
 
     }
