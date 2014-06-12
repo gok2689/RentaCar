9e90fc521a441614f8f967c65368018587d65baa
goktug <goktug.dulkan@gmail.com>
2014-06-13 01:38:23 +0300

    https://github.com/gok2689/RentaCar.git

--- RentaCarEntities/Member.cs
+++ RentaCarEntities/Member.cs
@@ -8,17 +8,18 @@ using System.ComponentModel.DataAnnotations;
 namespace RentaCarEntities
 {
    public class Member : IVersionable
-    {   [Required(ErrorMessage="İsim ve soyisim gerekli")]
+    {  
         [Display(Name="İsimSoyisim")]
         
         public string NameSurName { get; set; }
         [Required(ErrorMessage="Bir şifre giriniz")]
         [Display(Name="Şifre")]
-        [DataType(DataType.Password)]
+
         public string Password { get; set; }
         [Required(ErrorMessage="Bir email adressi giriniz ")]
         [Display(Name="Email")]
-        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+       
         public string Email { get; set; }
         public int Type { get; set; }
 
