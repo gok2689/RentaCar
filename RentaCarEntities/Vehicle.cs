c24278f04883bc48193cf9b80d8e973e51150e7e
goktug <goktug.dulkan@gmail.com>
2014-06-25 16:26:40 +0300

    https://github.com/gok2689/RentaCar.git

--- RentaCarEntities/Vehicle.cs
+++ RentaCarEntities/Vehicle.cs
@@ -20,16 +20,21 @@ namespace RentaCarEntities
         public int Id { get; set; }
         public bool IsDeleted { get; set; }
         public System.DateTime Version { get; set; }
+        [Display(Name="Plaka")]
         public string Plate { get; set; }
         public int BrandId { get; set; }
         public string Model { get; set; }
+        [Display(Name="Yakıt tipi")]
         public string FuelType { get; set; }
         public int BranchId { get; set; }
+        [Display(Name="Günlük kiralama bedeli")]
         public decimal PricePerDay { get; set; }
 
         [NotMapped]
+        [Display(Name="Marka")]
         public string _brandName { get; set; }
         [NotMapped]
+        [Display(Name="Şube")]
         public string _branchName { get; set; }
         public virtual ICollection<Event> Event { get; set; }
     }
