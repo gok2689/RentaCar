9620bd49894f4b0ee8df2494cda1ca652d55c167
goktug <goktug.dulkan@gmail.com>
2014-06-25 16:24:20 +0300

    https://github.com/gok2689/RentaCar.git

--- RentaCarEntities/Event.cs
+++ RentaCarEntities/Event.cs
@@ -20,10 +20,13 @@ namespace RentaCarEntities
         public int MemberId { get; set; }
         public int VehicleId { get; set; }
         
-
+        [Display(Name="Başlangıç tarihi")]
+        [DataType(DataType.DateTime)]
         public System.DateTime StartDate { get; set; }
-       
       
+        [Display(Name="Bitiş Tarihi")]
+        [DataType(DataType.DateTime)]
+
         public System.DateTime EndDate { get; set; }
 
         public virtual Member Member { get; set; }
